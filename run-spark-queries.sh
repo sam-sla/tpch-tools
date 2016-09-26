771a44700fd668af844244723637e0b54d812cf1
Samuel Almeida <sla@qlik.com>
2016-09-26 11:20:35 +0000

    Adds parameters and split into include/exclude behaviour

--- run-spark-queries.sh
+++ run-spark-queries.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 source_folder=$1
-exclude_pattern=$2
+DB=$2
+inclusivity=$3 # inc[lude] or exc[lude]
+match_pattern=$4
 
-DB="use tpch_flat_orc_100;"
+# DB="use tpch_flat_orc_100;"
+# DB="use tpch_flat_parquet_100;"
+use_DB="use $DB;"
 
 echo "source folder = $source_folder"
 
@@ -10,15 +14,19 @@ for query in $source_folder/tpch_query*.sql
 do
     query_name=$(basename "$query")
     echo "Query name is $query_name"
-#    echo "Exclude pattern is: $exclude_pattern"
 
-#    if ! [[ $query_name =~ $exclude_pattern ]] ; then # Excluding files
-    if [[ $query_name =~ $exclude_pattern ]] ; then   # Including files
-      #echo "BASH_REMATCH = $BASH_REMATCH"
-#      echo "Will process query $query_name"
-      echo -e "$DB\n$(cat $query)" > spark-$query_name
-      spark-sql -i testbench.settings -f spark-$query_name > $source_folder/sparkOutput/out-${query_name%.*} 2>&1
+    if [[$inclusivity =~ "inc"]] ; then
+        if [[ $query_name =~ $match_pattern ]] ; then   # Including files
+            echo -e "$DB\n$(cat $query)" > spark-$query_name
+            spark-sql -i testbench.settings -f spark-$query_name > $source_folder/sparkOutput-parquet/out-${query_name%.*} 2>&1
+        else
+            echo "Skipping query $query"
     else
-      echo "Skipping query $query"
+         if ! [[ $query_name =~ $match_pattern ]] ; then   # Excluding files
+            echo -e "$DB\n$(cat $query)" > spark-$query_name
+            spark-sql -i testbench.settings -f spark-$query_name > $source_folder/sparkOutput-parquet/out-${query_name%.*} 2>&1
+        else
+            echo "Skipping query $query"
+
     fi
 done
